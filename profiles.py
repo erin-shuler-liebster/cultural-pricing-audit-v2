3f6bf378264b9bddfdae2c862f9416be0008e8d6
erin-shuler-liebster <em.shlr@gmail.com>
2025-07-21 12:13:43 +0000

    ✅ Added profiles.py with Hofstede profile dictionary

---
+++ profiles.py
@@ -0,0 +1,8 @@
+
+HOFSTEDE_PROFILES = {
+    "Germany": {"PDI": 0.35, "IDV": 0.67, "UAI": 0.65, "MAS": 0.66, "LTO": 0.83, "IVR": 0.40},
+    "USA": {"PDI": 0.40, "IDV": 0.91, "UAI": 0.46, "MAS": 0.62, "LTO": 0.26, "IVR": 0.68},
+    "Japan": {"PDI": 0.54, "IDV": 0.46, "UAI": 0.92, "MAS": 0.95, "LTO": 0.88, "IVR": 0.42},
+    "India": {"PDI": 0.77, "IDV": 0.48, "UAI": 0.40, "MAS": 0.56, "LTO": 0.51, "IVR": 0.26},
+    "Sweden": {"PDI": 0.31, "IDV": 0.71, "UAI": 0.29, "MAS": 0.05, "LTO": 0.53, "IVR": 0.78},
+}
