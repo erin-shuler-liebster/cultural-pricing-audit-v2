084d6fe7713ba943bb2db1e88da53e6a4d1479e4
erin-shuler-liebster <em.shlr@gmail.com>
2025-07-21 16:49:53 +0200

    Update elasticity_simulator.py

--- elasticity_simulator.py
+++ elasticity_simulator.py
@@ -1,9 +1,1 @@
-# elasticity_simulator_tool.py
-import numpy as np
 
-def simulate_elasticity(base_price, elasticity, culture_sensitivity=1.0):
-    prices = np.linspace(base_price * 0.5, base_price * 1.5, 10)
-    demand = 100 * (1 - elasticity * ((prices - base_price) / base_price))
-    
-    adjusted_demand = demand * culture_sensitivity
-    return prices, adjusted_demand
